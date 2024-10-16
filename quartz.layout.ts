4f9fabd1d4e69c4c8c8d4b612b3d8a61ba2d0d18
W1-SK <masro.hax@gmail.com>
2024-10-16 21:20:52 +0200

    Quartz sync: Oct 16, 2024, 9:20 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,9 +30,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    Component.DesktopOnly(Component.Backlinks()),
     Component.MobileOnly(Component.Explorer()),  
   ],
 }
